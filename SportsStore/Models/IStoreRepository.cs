86dec41f0eff89cfaa1bc903e5b6774383ad6418
solarskit@student.agh.edu.pl <solarskit@student.agh.edu.pl>
2022-05-30 12:37:20 +0200

    database migration added

---
+++ SportsStore/Models/IStoreRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace SportsStore.Models
+{
+    //A class that depends on the IStoreRepository interface can obtain Product objects without needing to
+    //know the details of how they are stored or how the implementation class will deliver them.
+    public interface IStoreRepository
+    {
+        IQueryable<Product> Products { get; }
+        //represents the collection of objects that can be queried, such as those managed by a database
+    }
+
+}
--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<StoreDbContext>(opts => {
+    opts.UseSqlServer(
+    builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+});
+
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+//The AddScoped method creates a service where each HTTP request gets its own repository object, which
+//is the way that Entity Framework Core is typically used.
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
